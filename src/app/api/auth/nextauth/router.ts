7f9168ff81cff63eaea20253a27d3b0e2fba1f9f
ajeevsan <sanjeevmajhi036@gmail.com>
2025-06-06 13:49:26 +0530

    first commit

---
+++ src/app/api/auth/nextauth/router.ts
@@ -0,0 +1,32 @@
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma  from "../../../../lib/prisma";
+import bcrypt from "bcryptjs";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {}
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({ where: { email: credentials?.email } });
+        if (!user || !user.password) throw new Error("User not found");
+        const isValid = await bcrypt.compare(credentials!.password, user.password);
+        if (!isValid) throw new Error("Invalid password");
+        return user;
+      }
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
